fc9bba2b96f99e1d5507572f8fb5af74a6264c84
Triggered1234 <adrian.enache03@e-uvt.ro>
2025-04-09 20:06:52 +0300

    ex12

--- problema_1.c
+++ problema_1.c
@@ -2,4 +2,32 @@
 Varianta 17 - Problema 1
 1.	Cum sunt reprezentate si stocate argumentele de pe linia de comanda ? Dar variabilele de mediu transmise unui program C ? Mentionati valorile argc, argv si envp pentru urmatoarea linie:
 export X=2; export Y=3; ./a.out -a 2 -b 3
+
+argc (argument count) – numarul total de argumente, inclusiv numele programului
+argv (argument vector) – vector de char*, fiecare element fiind un argument, argv[0] este de obicei calea/numele programului (ex: "./problema1.out").
+envp (environment pointer) – vector de char* care contine variabilele de mediu în formatul "NUME=valoare".
+
+Variabilele de mediu sunt stocate in vectorul envp[] (sau accesibile prin functii ca getenv()),
+sunt setate inainte de executia programului (in shell, de ex. cu export).
+
+export X=2; export Y=3; ./a.out -a 2 -b 3
+
+X=2 și Y=3 sunt variabile de mediu.
+
+./a.out -a 2 -b 3 este executia programului cu 4 argumente suplimentare.
+
+argv:
+argv[0] = "./a.out"
+argv[1] = "-a"
+argv[2] = "2"
+argv[3] = "-b"
+argv[4] = "3"
+argv[5] = NULL
+argc = 5
+
+envp[0] = "X=2"
+envp[1] = "Y=3"
+urmate de celelalte variabile de mediu din sistem
+envp[n] = NULL
+
 */
--- problema_2.c
+++ problema_2.c
@@ -1,4 +1,14 @@
 /**
 Varianta 17 - Problema 2
 3.	Care este comanda care porneste un server TCP netcat pe adresa locala si pe portul 8080 ? Mentionati comanda si utilitatea fiecarei optiuni.
+
+nc -l 127.0.0.1 -p 8080
+
+nc – comanda pentru netcat, utilitar de retea, permite citirea si scrierea datelor prin conexiuni TCP sau UDP
+
+-l – din listen, indica faptul ca netcat trebuie sa asculte pentru conexiuni (adica sa functioneze ca un server).
+
+127.0.0.1 – adresa IP locala (localhost). Serverul va accepta conexiuni doar de la acelasi calculator.
+
+-p 8080 – specifica portul 8080 pe care serverul va asculta.
 */
