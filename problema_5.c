fa25c95b5649eff13c3e904a9e3fa08deb797547
rarespopa11 <raresnicholas@yahoo.com>
2025-04-09 19:53:23 +0300

    test pcd

---
+++ problema_5.c
@@ -0,0 +1,4 @@
+/**
+Varianta 17 - Problema 5
+2.	Scrieti un script C care creeaza un proces fiu. Procesul parinte va verifica existenta unui director temporar numit "temp_dir". Daca directorul nu exista, il va crea. Procesul parinte va muta fisierul de intrare specificat pe linia de comanda in acest director. Procesul fiu va apela, asupra programului head (cu optiunile -n 2), un apel din familia exec*, indicand calea absoluta a fisierului. Procesul parinte va astepta terminarea procesului fiu si va sterge directorul temporar.
+*/
